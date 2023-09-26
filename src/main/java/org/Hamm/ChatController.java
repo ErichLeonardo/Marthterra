bef05d0674c5d053dee5f177a8be8f435ea69546
ErichLeonardo <erich.hamm21@gmail.com>
2023-09-26 12:54:58 +0200

    chat

---
+++ src/main/java/org/Hamm/ChatController.java
@@ -0,0 +1,12 @@
+package org.Hamm;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class ChatController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+}

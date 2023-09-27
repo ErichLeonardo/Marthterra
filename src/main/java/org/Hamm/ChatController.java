180e1656b362f5ce61e0bcb0c3b7a24e5d039dc9
ErichLeonardo <erich.hamm21@gmail.com>
2023-09-27 10:40:59 +0200

    chat

--- src/main/java/org/Hamm/ChatController.java
+++ src/main/java/org/Hamm/ChatController.java
@@ -2,11 +2,34 @@ package org.Hamm;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class ChatController {
 
     @FXML
+    private Button primaryButton;
+
+    @FXML
+    private Button sendButton;
+
+    @FXML
+    private TextField textField;
+
+    @FXML
+    private TextArea textArea;
+    @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
     }
+
+    @FXML
+    private void handleSendButton() {
+        // Aquí puedes agregar el código que se ejecutará cuando se haga clic en el botón "Send".
+        // Por ejemplo, enviar el contenido del campo de texto al chat.
+        String message = textField.getText();
+        // Realiza alguna acción con el mensaje, como enviarlo al chat.
+    }
+
 }
